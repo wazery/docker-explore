d20e17acd232c2225d82088fb2ccfaccde7ddc22
Benjamin Foote <git@bnf.net>
2014-10-30 17:32:08 -0700

    fix case of cert since slack lower cases the name

--- 03_three/registry/install.sh
+++ 03_three/registry/install.sh
@@ -3,6 +3,6 @@
 
 mkdir /usr/local/share/ca-certificates/meedan-docker
 
-cp ca-certs/docker-regCA.crt /usr/local/share/ca-certificates/meedan-docker/
+cp ca-certs/docker-regca.crt /usr/local/share/ca-certificates/meedan-docker/
 
 update-ca-certificates
